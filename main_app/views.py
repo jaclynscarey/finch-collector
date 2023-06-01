ba06b3749e1e4d84739aa2487296e4482c8d5fa8
Jaclyn Carey <jsoyama@gmail.com>
2023-06-01 16:18:54 -0400

    wip: add finch data simulation in views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 
+finches = [
+    {
+        'species': 'Zebra Finch',
+        'color': 'Grey',
+        'beak_size': 'Small',
+        'location': 'Australia'
+    },
+    {
+        'species': 'Gouldian Finch',
+        'color': 'Multi-colored',
+        'beak_size': 'Small',
+        'location': 'Australia'
+    },
+    {
+        'species': 'Society Finch',
+        'color': 'White',
+        'beak_size': 'Small',
+        'location': 'United States'
+    },
+    {
+        'species': 'Java Sparrow',
+        'color': 'Grey',
+        'beak_size': 'Medium',
+        'location': 'Indonesia'
+    },
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
