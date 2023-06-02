f7370407ec7e6b77ba81cedccaf4303e6c160057
Jaclyn Carey <jsoyama@gmail.com>
2023-06-01 22:59:14 -0400

    wip: create finch model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    species = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    beak_size = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
