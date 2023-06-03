901c2750f7d9bfdf20f9ada1aa40b9417313952d
Jaclyn Carey <jsoyama@gmail.com>
2023-06-03 16:47:47 -0400

    wip: implement details page

--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,8 @@ def finches_index(request):
         'finches': finches
     })
 
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch,
+    })
