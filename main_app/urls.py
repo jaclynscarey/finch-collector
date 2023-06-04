b29ab63445cb7b78ac5c0f961e029da00661e9d3
Jaclyn Carey <jsoyama@gmail.com>
2023-06-04 00:41:51 -0400

    wip: associate and unassociate toys on details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+    path('finches/<int:finch_id>/unassoc_toy/<int:toy_id>/', views.unassoc_toy, name='unassoc_toy'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -20,10 +20,13 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    id_list = finch.toys.all().values_list('id')
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
     feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
         'finch': finch,
         'feeding_form': feeding_form,
+        'toys': toys_finch_doesnt_have,
     })
 
 def add_feeding(request, finch_id):
@@ -32,6 +35,14 @@ def add_feeding(request, finch_id):
         new_feeding = form.save(commit=False)
         new_feeding.finch_id = finch_id
         new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
     return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
